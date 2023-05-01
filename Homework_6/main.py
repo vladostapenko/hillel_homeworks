f277fe748a84b9e7dd3a342b81703531a9a3542f
Vlad Ostapenko <vladys.ostapenko@gmail.com>
2023-05-01 15:38:15 +0300

    Added main.py to run all the tasks in one time

---
+++ Homework_6/main.py
@@ -0,0 +1,16 @@
+# Всі завдання в окремому файлі, виклик всіх задач реалізувати у файлі main.py у конструкції if __name__ == '__main__':
+
+from Homework_6 import Task_1, Task_2, Task_3, Task_4, Task_5, Task_6
+
+
+def main():
+    Task_1
+    Task_2
+    Task_3
+    Task_4
+    Task_5
+    Task_6
+
+
+if __name__ == '__main__':
+    main()
